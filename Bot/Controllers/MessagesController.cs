93629f422ea2b24c311d14a17de6dc9d10339dda
Rayneel Kishore <Rayneel Kishore>
2016-11-24 19:30:34 +1300

    add updated files

--- Bot/Controllers/MessagesController.cs
+++ Bot/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,6 +8,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Bot.Models;
+using System.Collections.Generic;
 
 namespace Bot
 {
@@ -22,12 +25,148 @@ namespace Bot
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                string userMessage = activity.Text;
+
+
+                StateClient stateClient = activity.GetStateClient();
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+
+                bool isWeatherRequest = true;
+
+                string endOutput = "Hello";
+
+                // calculate something for us to return
+                if (userData.GetProperty<bool>("SentGreeting"))
+                {
+                    endOutput = "Hello again";
+                }
+                else
+                {
+                    userData.SetProperty<bool>("SentGreeting", true);
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                }
+
+
+
+                if (userMessage.Length > 9)
+                {
+                    if (userMessage.ToLower().Substring(0, 8).Equals("set home"))
+                    {
+                        string homeCity = userMessage.Substring(9);
+                        userData.SetProperty<string>("HomeCity", homeCity);
+                        await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                        endOutput = homeCity;
+                        isWeatherRequest = false;
+                    }
+                }
+
+                if (userMessage.ToLower().Contains("clear"))
+                {
+                    endOutput = "User data cleared";
+                    await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
+                    isWeatherRequest = false;
+                }
+                else if (userMessage.ToLower().Equals("home"))
+                {
+                    string homecity = userData.GetProperty<string>("HomeCity");
+                    if (homecity == null)
+                    {
+                        endOutput = "Home City not assigned";
+                        isWeatherRequest = false;
+                    }
+                    else
+                    {
+                        activity.Text = homecity;
+                    }
+                }
+
+                if (userMessage.ToLower().Equals("msa"))
+                {
+                    Activity replyToConversation = activity.CreateReply("MSA information");
+                    replyToConversation.Recipient = activity.From;
+                    replyToConversation.Type = "message";
+                    replyToConversation.Attachments = new List<Attachment>();
+                    List<CardImage> cardImages = new List<CardImage>();
+                    cardImages.Add(new CardImage(url: "https://cdn2.iconfinder.com/data/icons/ios-7-style-metro-ui-icons/512/MetroUI_iCloud.png"));
+                    List<CardAction> cardButtons = new List<CardAction>();
+                    CardAction plButton = new CardAction()
+                    {
+                        Value = "http://msa.ms",
+                        Type = "openUrl",
+                        Title = "MSA Website"
+                    };
+                    cardButtons.Add(plButton);
+                    ThumbnailCard plCard = new ThumbnailCard()
+                    {
+                        Title = "Visit MSA",
+                        Subtitle = "The MSA Website is here",
+                        Images = cardImages,
+                        Buttons = cardButtons
+                    };
+                    Attachment plAttachment = plCard.ToAttachment();
+                    replyToConversation.Attachments.Add(plAttachment);
+                    await connector.Conversations.SendToConversationAsync(replyToConversation);
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+
+                }
+                if (!isWeatherRequest)
+                {
+                    // return our reply to the user
+                    Activity infoReply = activity.CreateReply(endOutput);
+
+                    await connector.Conversations.ReplyToActivityAsync(infoReply);
+
+                }
+                else
+                {
+
+                    MainClass.RootObject rootObject;
+
+                    //    Console.WriteLine(activity.Attachments[0].ContentUrl);
+
+                    HttpClient client = new HttpClient();
+                    string x = await client.GetStringAsync(new Uri("http://api.openweathermap.org/data/2.5/weather?q=" + activity.Text + "&units=metric&APPID=440e3d0ee33a977c5e2fff6bc12448ee"));
+
+                    rootObject = JsonConvert.DeserializeObject<MainClass.RootObject>(x);
+
+                    string cityName = rootObject.name;
+                    string temp = rootObject.main.temp + "°C";
+                    string pressure = rootObject.main.pressure + "hPa";
+                    string humidity = rootObject.main.humidity + "%";
+                    string wind = rootObject.wind.deg + "°";
+                    // added fields
+                    string icon = rootObject.weather[0].icon;
+                    int cityId = rootObject.id;
+
+                    // return our reply to the user
+                    Activity replyToConversation = activity.CreateReply($"Current weather for {cityName}");
+                    replyToConversation.Recipient = activity.From;
+                    replyToConversation.Type = "message";
+                    replyToConversation.Attachments = new List<Attachment>();
+                    List<CardImage> cardImages = new List<CardImage>();
+                    cardImages.Add(new CardImage(url: "http://openweathermap.org/img/w/" + icon + ".png"));
+                    List<CardAction> cardButtons = new List<CardAction>();
+                    CardAction plButton = new CardAction()
+                    {
+                        Value = "https://openweathermap.org/city/" + cityId,
+                        Type = "openUrl",
+                        Title = "More Info"
+                    };
+                    cardButtons.Add(plButton);
+                    ThumbnailCard plCard = new ThumbnailCard()
+                    {
+                        Title = cityName + " Weather",
+                        Subtitle = "Temperature " + temp + ", pressure " + pressure + ", humidity  " + humidity + ", wind speeds of " + wind,
+                        Images = cardImages,
+                        Buttons = cardButtons
+                    };
+                    Attachment plAttachment = plCard.ToAttachment();
+                    replyToConversation.Attachments.Add(plAttachment);
+                    await connector.Conversations.SendToConversationAsync(replyToConversation);
+
+                }
             }
             else
             {
